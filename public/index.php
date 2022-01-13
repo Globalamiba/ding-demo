1006190c3ad171e0e0c202f136de8b3d54cc2d1e
Wj <314047179@163.com>
2022-01-13 15:34:41 +0800

    test run

--- public/index.php
+++ public/index.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 use Phalcon\Exception;
 use ding\Bootstrap;
 
-require "vendor/autoload.php";
-
 define('APP_PATH', realpath('..'));
+require_once APP_PATH . "/vendor/autoload.php";
+
 try {
     $boot = new Bootstrap(APP_PATH);
     $boot->run();
