e86454352505335452f7e17992627f1a86119667
Wj <314047179@163.com>
2022-01-18 15:47:07 +0800

    add log

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -10,6 +10,7 @@ class AuthController extends BaseController
     public function indexAction()
     {
         $code = $this->request->get('authCode');
+        $this->logger->info("url:".$_SERVER['SERVER_NAME'].':'.$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"]);
         $this->logger->info("dingding code:".$code);
         $token = dingTalkService::getUserToken($code, $this->config->path('ding.AppKey'), $this->config->path('ding.AppSecret'));
         $this->logger->info("user token:".json_encode($token));
