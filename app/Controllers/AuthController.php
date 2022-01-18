5bcd13d106c69aa00352fbc45b2825dd46b41682
Wj <314047179@163.com>
2022-01-18 15:11:29 +0800

    add log

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -10,9 +10,12 @@ class AuthController extends BaseController
     public function indexAction()
     {
         $code = $this->request->get('authCode');
+        $this->logger->info("dingding code:".$code);
         $token = dingTalkService::getUserToken($code, $this->config->path('ding.AppKey'), $this->config->path('ding.AppSecret'));
+        $this->logger->info("user token:".json_encode($token));
         $this->cache->set('user_token', $token->accessToken);
         $userinfo = dingTalkService::getUserInfoWithToken($token->accessToken);
+        $this->logger->info("user token:".json_encode($userinfo));
         $this->cache->set('userInfo', $userinfo);
         $this->response->redirect('/index/index');
     }
