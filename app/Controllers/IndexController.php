a4f02873d0d56faf66dfe027294b3afaf2b23ce8
Wj <314047179@163.com>
2022-01-18 15:44:49 +0800

    add log

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -11,7 +11,7 @@ class IndexController extends BaseController
     {
         if (!$this->cache->has('userinfo')) {
             $params = [
-                'redirect_url' => urlencode('http://183.136.151.130:8080/Auth'),
+                'redirect_uri' => urlencode('http://183.136.151.130:8080/Auth'),
                 'response_type' => 'code',
                 'client_id' => $this->config->path('ding.AppKey'),
                 'scope' => 'openid',
